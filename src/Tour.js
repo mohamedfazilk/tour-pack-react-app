fff63771af1a39e8ecf4096db9ae32e264135962
mohamedfazilk <2114921@cognizant.com>
2022-07-11 15:50:46 +0530

    first structure of tours app

---
+++ src/Tour.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tour =()=> {
+  return (
+    <div>single Tour</div>
+  )
+}
+
+export default Tour
---
+++ src/Tours.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Tour from './Tour'
+
+const Tours =() =>{
+  return (
+    <div>Tours</div>
+  )
+}
+
+export default Tours
