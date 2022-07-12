95c3f7c1b98761bd5b0caa71c4b1bc710f31f066
mohamedfazilk <2114921@cognizant.com>
2022-07-12 15:24:27 +0530

    creating a state for readmore and setting initially as false. it wil show all the contents if it is true otherwise it will restrict limited words

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Tour =({id,image,info,price,name})=> {
+  //initially readmore is setting false//
+  const [readMore, setReadMore] = useState(false);
   return (
+
     <article className='single-tour'>
       <img src={image} alt={name}/>
       <footer>
@@ -9,7 +12,9 @@ const Tour =({id,image,info,price,name})=> {
           <h4>{name}</h4>
           <h4 className='tour-price'>${price}</h4>
         </div>
-        <p>{info}</p>
+        
+       {/* readmore is true then show whole text otherwise restrict to 0-200 words */}
+        <p>{readMore ? info:`${info.substring(0,200)}.....`}</p>
         <button className='delete-btn'>Not Interested</button>
       </footer>
     </article>
